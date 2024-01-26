8e89e24ba51c799127a9ddabc5cd7eee9e51076a
matthewkuria <mathewkwachira@gmail.com>
2024-01-26 21:23:58 +0300

    add Links to the Router capabilty

--- my-project/src/components/NavMenu.jsx
+++ my-project/src/components/NavMenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function NavMenu(){
     return(
         <div className="MainDiv hidden md:flex  justify-between lg:flex-col">
