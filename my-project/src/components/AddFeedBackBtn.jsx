16c011d29973d00c3b1358f5f3a409291d984667
matthewkuria <mathewkwachira@gmail.com>
2024-01-29 22:43:49 +0300

    add Go back link colr

--- my-project/src/components/AddFeedBackBtn.jsx
+++ my-project/src/components/AddFeedBackBtn.jsx
@@ -14,8 +14,8 @@ export default function AddButton(){
         {
             isActive &&
             <div className=" absolute top-0 left-0 bg-[#F7F8FD]  right-0 bottom-0  ">
-                <div className="flex justify-center items-center">                    
-                    <a href="">
+                <div className="flex justify-center mx-auto items-center">                    
+                    <a href="" className="text-[#647196]">
                         <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
                         <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" stroke-width="3"/>
                         </svg>
