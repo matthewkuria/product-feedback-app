00cd209e49da5a59925972666b04ff2b36eecd4c
matthewkuria <mathewkwachira@gmail.com>
2024-01-29 22:59:34 +0300

    more format to the Go back button

--- my-project/src/components/AddFeedBackBtn.jsx
+++ my-project/src/components/AddFeedBackBtn.jsx
@@ -13,17 +13,19 @@ export default function AddButton(){
         </button>
         {
             isActive &&
-            <div className=" absolute top-0 left-0 bg-[#F7F8FD]  right-0 bottom-0  ">
-                <div className="flex justify-center mx-auto items-center">                    
-                    <a href="" className="text-[#647196]">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
-                        <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" stroke-width="3"/>
-                        </svg>
-                        Go Back
-                    </a>
-                </div>
-                <div className="flex justify-center mt-10 md:w-[768px] mx-auto">
-                    <AddFeedBack />
+            <div className=" absolute top-0 left-0 bg-[#F7F8FD]  right-0 bottom-0  ">                
+                <div className="relative  bg-[#F7F8FD]">
+                    <div className="">                    
+                        <a href="" className="text-[#647196] flex items-center justify-center mt-5 mb-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
+                            <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" stroke-width="2"/>
+                            </svg>
+                            <p>Go Back</p>
+                        </a>
+                    </div>
+                    <div className="flex justify-center mt-10 md:w-[768px] mx-auto">
+                        <AddFeedBack />
+                    </div>
                 </div>
             </div>
         }
