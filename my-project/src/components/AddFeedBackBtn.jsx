b686fb0a5a1531d9cff61d78bc11b6213bb3e87d
matthewkuria <mathewkwachira@gmail.com>
2024-01-30 07:34:19 +0300

    add react-router useNavigate

--- my-project/src/components/AddFeedBackBtn.jsx
+++ my-project/src/components/AddFeedBackBtn.jsx
@@ -1,12 +1,12 @@
 import {React, useState} from "react";
-import {  Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AddFeedBack from "./AddFeedBack";
 export default function AddButton(){
     const [isActive , setActive] = useState(false)
     function handleClick(){
         setActive(!isActive)
-
     }
+    const navigate = useNavigate();
     return(
         <>
         <button onClick={handleClick} className="BTN w-36 h-10 text-white hover:bg-[#C75AF6]">
@@ -17,12 +17,12 @@ export default function AddButton(){
             <div className=" absolute top-0 left-0 bg-[#F7F8FD]  right-0 bottom-0  ">                
                 <div className="relative  bg-[#F7F8FD]">
                     <div className="">                    
-                        <Link to="/all" className="text-[#647196] flex items-center justify-center mt-5 mb-0">
+                        <button onClick={() => navigate(-2)} className="text-[#647196] flex items-center justify-center mt-5 mb-0">
                             <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
                             <path d="M4.33447 9L0.334473 5L4.33447 1" stroke="#4661E6" stroke-width="2"/>
                             </svg>
                             <p>Go Back</p>
-                        </Link>
+                        </button>
                     </div>
                     <div className="flex justify-center mt-10 md:w-[768px] mx-auto">
                         <AddFeedBack />
